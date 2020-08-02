a67fd1810f9a3cd4f4eeb9222dbf884f47081863
jas.1998818 <248578737@qq.com>
2020-08-02 16:05:40 +0800

    单例模式_懒汉式多线程测试

--- 07_设计模式_单例模式/src/singleton/b_懒汉式同步方法/Client.java
+++ 07_设计模式_单例模式/src/singleton/b_懒汉式同步方法/Client.java
@@ -2,7 +2,8 @@ package singleton.b_懒汉式同步方法;
 
 public class Client {
     public static void main(String[] args) {
-        //此时无论执行多少次都是同一个对象
+        //此时无论执行多少次都是同一个对象,这里线程执行时是阻塞的,可以使用CountDownLatch来统计执行时间
+        // TODO: 2020/8/2 加上CountDownLatch来统计执行时间
         for (int i = 0; i < 10; i++) {
             new Thread(() -> System.out.println(Singleton.getInstance().hashCode())).start();
         }
