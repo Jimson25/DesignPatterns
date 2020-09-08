e49b9fc7a25657761746613e5b7337fcdb3e82b9
金傲松 <jinaosong@beidougx.com>
2020-09-08 19:25:57 +0800

    门面模式引入

---
+++ 23_设计模式_门面模式/src/Demo_01/LetterProcessImpl.java
@@ -0,0 +1,23 @@
+package Demo_01;
+
+public class LetterProcessImpl implements ILetterProcess {
+    @Override
+    public void writeContext(String context) {
+        System.out.println("写信的内容是： " + context);
+    }
+
+    @Override
+    public void fillEnvelope(String address) {
+        System.out.println("收件人地址是：" + address);
+    }
+
+    @Override
+    public void letterIntoEnvelope() {
+        System.out.println("把信放到信封。。。。。");
+    }
+
+    @Override
+    public void sendLetter() {
+        System.out.println("前往邮局~~~~");
+    }
+}
