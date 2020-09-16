4b62926fc517991bcaf28dade35e5d4bb081ef1a
Jimson25 <248578737@qq.com>
2020-09-16 19:28:48 +0800

    多状态备忘录模式，未完

--- 24_设计模式_备忘录模式/src/extension_03/BeanUtils.java
+++ 24_设计模式_备忘录模式/src/extension_03/BeanUtils.java
@@ -1,58 +1,62 @@
 package extension_03;
 
 import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
 import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 
 public class BeanUtils {
     public static HashMap<String, Object> backupProp(Object bean) {
         //把bean的所有属性及数值放入到Hashmap中
-        public static HashMap<String, Object> backupProp (Object bean){
-            HashMap<String, Object> result = new HashMap<String, Object>();
-            try {
-                //获得Bean描述
-                BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass());
-                //获得属性描述
-                PropertyDescriptor[] descriptors = beanInfo.getPropertyDescriptors();
-                //遍历所有属性
-                for (PropertyDescriptor des : descriptors) {
-                    //属性名称
-                    String fieldName = des.getName();
-                    //读取属性的方法
-                    Method getter = des.getReadMethod();
-                    //读取属性值
-                    Object fieldValue = getter.invoke(bean, new Object[]{});
-                    if (!fieldName.equalsIgnoreCase("class")) {
-                        result.put(fieldName, fieldValue);
-                    }
+        HashMap<String, Object> map = new HashMap<>();
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass());
+            PropertyDescriptor[] descriptors = beanInfo.getPropertyDescriptors();
+            for (PropertyDescriptor des : descriptors) {
+                //属性名称
+                String fieldName = des.getName();
+                //读取属性的方法
+                Method getter = des.getWriteMethod();
+                Object invoke = getter.invoke(bean, new Object() {
+                });
+                //如果有这个属性
+                if (!fieldName.equalsIgnoreCase("class")) {
+                    map.put(fieldName, invoke);
                 }
-            } catch (Exception e) {
-                //异常处理
             }
-            return result;
+
+
+        } catch (IntrospectionException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
         }
 
-        public static void restoreProp (Object bean, HashMap < String, Object > propMap){
-            try {
-//获得Bean描述
-                BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass());
-//获得属性描述
-                PropertyDescriptor[] descriptors = beanInfo.getPropertyDescriptors();
-//遍历所有属性
-                for(PropertyDescriptor des:descriptors){
-//属性名称
-                    String fieldName = des.getName();
-//如果有这个属性
-                    if(propMap.containsKey(fieldName)){
-//写属性的方法
-                        Method setter = des.getWriteMethod();
-                        setter.invoke(bean, new Object[]{propMap.get(fieldName)});
-                    }
+        return map;
+    }
+
+    public static void restoreProp(Object bean, HashMap<String, Object> propMap) {
+        try {
+            //获得Bean描述
+            BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass());
+            //获得属性描述
+            PropertyDescriptor[] descriptors = beanInfo.getPropertyDescriptors();
+            //遍历所有属性
+            for (PropertyDescriptor des : descriptors) {
+                //属性名称
+                String fieldName = des.getName();
+                //如果有这个属性
+                if (propMap.containsKey(fieldName)) {
+                    //写属性的方法
+                    Method setter = des.getWriteMethod();
+                    setter.invoke(bean, new Object[]{propMap.get(fieldName)});
                 }
-            } catch (Exception e) {
-//异常处理
-                System.out.println("shit");
-                e.printStackTrace();
             }
+        } catch (Exception e) {
+            //异常处理
+            System.out.println("shit");
+            e.printStackTrace();
         }
     }
+}
--- 24_设计模式_备忘录模式/src/extension_03/Memento.java
+++ 24_设计模式_备忘录模式/src/extension_03/Memento.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 public class Memento {
     private HashMap<String, Object> stateMap = new HashMap<>();
 
+    public Memento(HashMap<String, Object> stateMap) {
+        this.stateMap = stateMap;
+    }
+
     public HashMap<String, Object> getStateMap() {
         return stateMap;
     }
